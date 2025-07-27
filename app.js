8acafb16e1a458243ebc8a623439c05420a1bc30
Your New Name <your.email@example.com>
2025-07-27 15:39:46 +0530

    black

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send(`
-        <div style="background-color:red; color:white; height:100vh; display:flex; justify-content:center; align-items:center; font-size:40px;">
-            RED 
+        <div style="background-color:black; color:white; height:100vh; display:flex; justify-content:center; align-items:center; font-size:40px;">
+            BLACK
         </div>
     `);
 });
 
 app.listen(port, () => {
-    console.log(`RED deployment server running on port ${port}`);
+    console.log(`BLACK deployment server running on port ${port}`);
 });

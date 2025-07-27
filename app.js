61201cf0e2e3a868835e5a09b4197ce398b0beb8
Your New Name <your.email@example.com>
2025-07-27 15:59:09 +0530

    purple

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send(`
-        <div style="background-color:pink; color:white; height:100vh; display:flex; justify-content:center; align-items:center; font-size:40px;">
-            PINK
+        <div style="background-color:purple; color:white; height:100vh; display:flex; justify-content:center; align-items:center; font-size:40px;">
+            Purple
         </div>
     `);
 });
 
 app.listen(port, () => {
-    console.log(`BLACK deployment server running on port ${port}`);
+    console.log(`Purple deployment server running on port ${port}`);
 });

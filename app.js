57f7cb4b1a2c80244bfebdd55a81fe3a98f3ffde
Your New Name <your.email@example.com>
2025-07-27 14:12:16 +0530

    Dockerfile

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send(`
-        <div style="background-color:black; color:white; height:100vh; display:flex; justify-content:center; align-items:center; font-size:40px;">
-            Black Deployment hello chandan bhai
+        <div style="background-color:white; color:white; height:100vh; display:flex; justify-content:center; align-items:center; font-size:40px;">
+            White Deployment hello chandan bhai
         </div>
     `);
 });
 
 app.listen(port, () => {
-    console.log(`Black deployment server running on port ${port}`);
+    console.log(`white deployment server running on port ${port}`);
 });

82c70b32359b60efd38d0039d4910b5a2482937d
Your New Name <your.email@example.com>
2025-07-27 18:38:21 +0530

    yellow

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send(`
-        <div style="background-color:orange; color:white; height:100vh; display:flex; justify-content:center; align-items:center; font-size:40px;">
-            Purple
+        <div style="background-color:yellow; color:white; height:100vh; display:flex; justify-content:center; align-items:center; font-size:40px;">
+            YELLOW
         </div>
     `);
 });
